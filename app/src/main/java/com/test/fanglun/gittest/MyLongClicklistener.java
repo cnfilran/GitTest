cc1f5917b30f3d5adc931947493dcc8bdd975df8
cnfilran <m13758189430@163.com>
2016-10-08 21:38:04 +0800

    RecycleView

---
+++ app/src/main/java/com/test/fanglun/gittest/MyLongClicklistener.java
@@ -0,0 +1,11 @@
+package com.test.fanglun.gittest;
+
+import android.view.View;
+
+/**
+ * Created by fanglun on 2016/10/8.
+ */
+public interface MyLongClicklistener {
+
+    void onLongClicklistener(View view,int position);
+}
